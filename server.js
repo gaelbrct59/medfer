7a90ee3846de6220164bc5a17a61da04f3901769
gaelbrct59 <gael.brct@gmail.com>
2021-05-10 14:08:39 +0200

    try to send connect client to server

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('/*', function(req,res) {
 });
 
 // Start the app by listening on the default Heroku port
-const port = process.env.PORT || 8080;
+const port = 2121;
 app.listen(process.env.PORT || 8080);
 const server = http.createServer(express);
 const webSocketServer = new WebSocket.Server( {server} )
